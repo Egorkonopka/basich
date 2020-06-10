317ca950c5b5552f1af19f304432d7831942c8ac
egor <egorkonopka93@gmail.com>
2020-06-10 17:06:11 +0300

    yes

--- web/js/main.js
+++ web/js/main.js
@@ -8,9 +8,54 @@ if ($.fn.slider) {
         speed: 300
     });
 
-  function showCart(cart){
+    function showCart(cart){
         $('#cart .modal-body').html(cart);
         $('#cart').modal();
+    }
+
+    function getCart(){
+        $.ajax({
+            url: '/cart/show',
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
+        return false;
+    }
+
+    $('#cart .modal-body').on('click', '.del-item', function(){
+        var id = $(this).data('id');
+        $.ajax({
+            url: '/cart/del-item',
+            data: {id: id},
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
+    });
+
+    function clearCart(){
+        $.ajax({
+            url: '/cart/clear',
+            type: 'GET',
+            success: function(res){
+                if(!res) alert('Ошибка!');
+                showCart(res);
+            },
+            error: function(){
+                alert('Error!');
+            }
+        });
     }
 
      $('.add-to-cart').on('click', function (e) {
